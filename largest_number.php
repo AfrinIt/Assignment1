76798902bab127ca5ee3b84ff26107b2451a7378
Naba Afrin <naba.facile63@gmail.com>
2024-07-08 23:45:25 +0600

    Assigment 01

---
+++ largest_number.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Finding Largest Number</h1>
+
+<?php
+$num1 = 4; 
+$num2 = 5;
+$num3 = 6;
+if($num1>$num2 $ $num1!=$num2){
+    echo"$num1 is the largest number";
+}
+elseif($num1>$num3 $ $num1!=$num3){
+    echo"$num1 is the largest number";
+}
+elseif($num2>$num1 $ $num1!=$num2){
+    echo"$num2 is the largest number";
+}
+
+elseif($num2>$num3 $ $num3!=$num2){
+    echo"$num2 is the largest number";
+}
+elseif($num3>$num1 $ $num1!=$num3){
+    echo"$num3 is the largest number";
+}
+elseif($num3>$num2 $ $num3!=$num2 ){
+    echo"$num3 is the largest number";
+}
+?>
+
+</body>
+</html>
+
